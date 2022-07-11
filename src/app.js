ea52088b2e23acd719cc054d3dade9b1faa7d802
Anastasiaheralt <rosimarherrerao@gmail.com>
2022-07-11 05:18:22 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["el", "nuestro", "su", "una", "un"];
+let adj = ["gran", "genial", "super"];
+let noun = ["cat", "corredor", "decorador"];
+
+function domainGenerator(pronoun, adj, noun) {
+  let a = Math.floor(Math.random() * pronoun.length);
+  let b = Math.floor(Math.random() * adj.length);
+  let c = Math.floor(Math.random() * noun.length);
+
+  return pronoun[a] + " " + adj[b] + " " + noun[c] + "" + ".com ";
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var domain = document.getElementById("domain");
+  domain.innerHTML = domainGenerator(pronoun, adj, noun);
 };
+
+window.onload = onload;
